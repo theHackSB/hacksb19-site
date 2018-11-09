5d29093edac1d5b7a3827d4e357fac79bceff727
Sethu Senthil <sethunarayansenthil@gmail.com>
2018-11-08 20:57:41 -0500

    Updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-log('scripts.js loaded')
+log('scripts.js loaded');
+let scroll = new SmoothScroll('a[href*="#"]');
+let burger = doc.querySelector('.burger');
+let nav = doc.querySelector('#'+burger.dataset.target);
+
+    burger.addEventListener('click',() => {
+      burger.classList.toggle('is-active');
+      nav.classList.toggle('is-active');
+    });
