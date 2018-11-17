dfd31e9a00d2429b05b2565e922c5d4d3533436d
Sethu Senthil <sethunarayansenthil@gmail.com>
2018-11-17 13:56:13 -0500

    added MLH oathu and Gcal support

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,10 @@ let nav = doc.querySelector('#'+burger.dataset.target);
       burger.classList.toggle('is-active');
       nav.classList.toggle('is-active');
     });
+
+    let signIn = () =>{
+      window.open('https://my.mlh.io/oauth/authorize?client_id=a5d42319c720d586e0ca1047e9ba7b307610c4ceafd982401cf7b66bde301385&redirect_uri=https%3A%2F%2F${window.location.href}&response_type=token')
+    }
+    let addtoCal = () =>{
+      window.open('https://calendar.google.com/event?action=TEMPLATE&tmeid=NnNpYmQwdHVxdGFvdjk2OWEwdmxhYWJxZ24gc2Jjb21wc2NpY2x1YkBt&tmsrc=sbcompsciclub%40gmail.com')
+    }
