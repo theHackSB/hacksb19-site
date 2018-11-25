c8379181aff1fae47b25add54b45cde1768554a5
Sethu Senthil <sethunarayansenthil@gmail.com>
2018-11-25 13:06:00 -0500

    faster js, added FAQ, removed unused files

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 log('scripts.js loaded');
-let scroll = new SmoothScroll('a[href*="#"]');
-let burger = doc.querySelector('.burger');
-let nav = doc.querySelector('#'+burger.dataset.target);
+let scroll = new SmoothScroll('a[href*="#"]'),
+ burger = doc.querySelector('.burger'),
+  nav = doc.querySelector('#'+burger.dataset.target);
 
     burger.addEventListener('click',() => {
       burger.classList.toggle('is-active');
@@ -10,7 +10,7 @@ let nav = doc.querySelector('#'+burger.dataset.target);
 
     let signIn = () =>{
       window.open('https://my.mlh.io/oauth/authorize?client_id=a5d42319c720d586e0ca1047e9ba7b307610c4ceafd982401cf7b66bde301385&redirect_uri=https%3A%2F%2F${window.location.href}&response_type=token')
-    }
-    let addtoCal = () =>{
+    },
+     addtoCal = () =>{
       window.open('https://calendar.google.com/event?action=TEMPLATE&tmeid=NnNpYmQwdHVxdGFvdjk2OWEwdmxhYWJxZ24gc2Jjb21wc2NpY2x1YkBt&tmsrc=sbcompsciclub%40gmail.com')
     }
